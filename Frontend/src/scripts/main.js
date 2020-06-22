9805d2d5d70a0883f65a5b55609482aeb0f1f520
jgasparino <jgasparino@bvirtual.com>
2020-06-22 22:32:48 +0200

    update beautify

--- Frontend/src/scripts/main.js
+++ Frontend/src/scripts/main.js
@@ -1,41 +1,41 @@
 import main from '../UX/styles/main.scss';
 
 function deactivateDevTool() {
-    window.oncontextmenu = () => {
-        return false;
-    }
-    document.onkeydown = (e) => {
-        if (window.event.keyCode == 123 || e.button == 2)
-            return false;
-    }
+	window.oncontextmenu = () => {
+		return false;
+	}
+	document.onkeydown = (e) => {
+		if (window.event.keyCode == 123 || e.button == 2)
+			return false;
+	}
 }
 
 function eventHandler() {
 
-    let signalParams = {
-        message: '',
-        freq: 0
-    }
+	let signalParams = {
+		message: '',
+		freq: 0
+	}
 
-    document.getElementById("message").addEventListener("change", (input) => {
-        signalParams.message = input.target.value;
-    });
-    document.getElementById("freq").addEventListener("change", (input) => {
-        signalParams.freq = +input.target.value;
-    });
-    document.getElementById("btn_sendMessage").addEventListener("click", (e) => {
-        e.preventDefault();
-        showSignal(signalParams);
-    });
+	document.getElementById("message").addEventListener("change", (input) => {
+		signalParams.message = input.target.value;
+	});
+	document.getElementById("freq").addEventListener("change", (input) => {
+		signalParams.freq = +input.target.value;
+	});
+	document.getElementById("btn_sendMessage").addEventListener("click", (e) => {
+		e.preventDefault();
+		showSignal(signalParams);
+	});
 
 }
 
 function showSignal(signalParams) {
 
-    // eel.testing("testing eel")((data)=>{
-    //     console.log(data)
-    // });
-    eel.main(signalParams);
+	// eel.testing("testing eel")((data)=>{
+	//     console.log(data)
+	// });
+	eel.main(signalParams);
 }
 
 //deactivateDevTool();
